9ecd7165f71cb9dae2d1dfca26169e7e2d076cb9
wesinalves <wesinec@gmail.com>
2019-07-10 16:23:56 -0300

    adding codes directory

---
+++ codigos/cap1/hello.py
@@ -0,0 +1,3 @@
+# meu primeiro programa 
+# em python - Hello world
+print("Hello", "World")
---
+++ codigos/cap4/fibonacci.py
@@ -0,0 +1,9 @@
+def fibonacci(number):
+
+    if number < 0:
+        print('Valor negativo é invalido')
+    
+    if number == 0 or number == 1:
+        return 1
+    else:
+        return fibonacci(number - 1) + fibonacci(number - 2)
