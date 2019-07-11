0841c26bacd853b08a9bd30c8cb6edaaf738975e
wesinalves <wesinec@gmail.com>
2019-07-11 17:09:58 -0300

    add sequence example

---
+++ codigos/cap4/creatingSequences.py
@@ -0,0 +1,11 @@
+string1 = ""
+
+lista1 = []
+lista2 = [1,2,3]
+
+tupla1 = ()
+tupla2 = 1,2,3
+tupla3 = (1,2,3)
+singleton = 1,
+
+
---
+++ codigos/cap4/phones.py
@@ -0,0 +1,8 @@
+telefones = {'joao':'9999-9888','maria':'9999-8888',
+    'jose':'9999-7777'}
+
+telefones['joao']
+
+telefones['pedro'] = '9999-6767'
+
+del telefones['maria']
