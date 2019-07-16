f3a5bebb079ac8095b66698db70acbc4d13aca25
wesinalves <wesinec@gmail.com>
2019-07-16 17:08:43 -0300

    cap7 examples

--- codigos/cap6/guitar.py
+++ codigos/cap6/guitar.py
@@ -1,6 +1,22 @@
 class Guitar:
     def __init__(self):
         self._brand = 'Taylor'
-        self._body = 'grand concert'
+        self._body = 'solid'
+        self._shape = 'grand concert'
         self._numStrings = 6
+        self._model = '314ce'
+        self._strings = []
+        self._price = 2000
+
+    def play(self, style, bpm, music):
+        return "Let's play {} at {} bpm using {} style with {} {}".format(music, bpm, style, self._brand, self._model)
+    
+    def tuneStrings(self, afination=['E','A','D','G','B','e']):
+        self._strings = afination
+    
+    def checkStrings(self):
+        if len(self._strings) == 0:
+            print("Guitar is not tuned!")
+        else:
+            print(self._strings)
 
--- codigos/cap6/guitar_obj.py
+++ codigos/cap6/guitar_obj.py
@@ -2,3 +2,10 @@ from guitar import Guitar
 
 g = Guitar()
 print(g._brand, g._body)
+print(g.play("strumming","98","Eu sou calebe"))
+
+g.checkStrings()
+g.tuneStrings()
+g.checkStrings()
+g.tuneStrings(['D','A','D','G','A','d'])
+g.checkStrings()
