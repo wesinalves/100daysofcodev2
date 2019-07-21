1334a58bebb9986ea3e2545c7c593ba4825dd41b
wesinalves <wesinec@gmail.com>
2019-07-21 11:51:25 -0300

    string formatting example

---
+++ codigos/cap2/string_formatting.py
@@ -0,0 +1,13 @@
+# Jornada Python - Capítulo 2
+# String formatting demonstration
+
+print("O Senhor é meu pastor, nada me faltará. 'Salmo 23:1'")
+print("O Senhor é meu pastor, nada me faltará. \"Salmo 23:1\"")
+print('O Senhor é meu pastor, nada me faltará. \'Salmo 23:1\'')
+print('O Senhor é meu pastor, nada me faltará. "Salmo 23:1"')
+
+integerValue = 4237.05
+print("Integer", integerValue)
+print("decimal Integer {:.0f}".format(integerValue))
+print("Hexadecimal integer {:x}\n".format(int(integerValue)))
+
