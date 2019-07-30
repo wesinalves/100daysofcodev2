06c9272a20ca5d6b69cc7b9005cceacc7ed9c379
wesinalves <wesinec@gmail.com>
2019-07-30 16:29:04 -0300

    add square function example

---
+++ codigos/cap4/square.py
@@ -0,0 +1,9 @@
+# Journey Python - Chapter 4
+# Creating and using a programmer-defined function.
+
+# function definition
+def square(y):
+    return y * y
+
+for x in range(1, 11):
+    print(square(x))
