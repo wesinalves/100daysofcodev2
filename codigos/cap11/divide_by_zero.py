c8f7d83ac3c650c1ecbc46f99f195a377d5e4191
wesinalves <wesinec@gmail.com>
2019-08-13 15:58:15 -0300

    divide by zero example of exception handler

---
+++ codigos/cap11/divide_by_zero.py
@@ -0,0 +1,23 @@
+# Python Journey - Chapter 11
+# Simple exception handling example.
+
+number1 = input("Enter numerator: ")
+number2 = input("Enter denominator: ")
+
+# attempt to convert and divide values
+try:
+    number1 = float(number1)
+    number2 = float(number2)
+    result = number1 / number2
+
+# float raises a ValueError exception
+except ValueError:
+    print("You must enter two numbers")
+
+# division by zero raises a ZeroDivisionError exception
+except ZeroDivisionError:
+    print("Dividing by zero is not allowed")
+
+# else clause's suite executes if try suite raises no exceptions
+else:
+    print("{:.3f} / {:.3f} = {:.3f}".format(number1, number2, result))
