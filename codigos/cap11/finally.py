2b3ac0da15dee4e448860f37292f3ca8e787abef
wesinalves <wesinec@gmail.com>
2019-08-14 19:02:03 -0300

    add finally example

---
+++ codigos/cap11/finally.py
@@ -0,0 +1,37 @@
+# Python Journey - Chapter 11
+# Using finally clauses.
+
+def doNotRaiseException():
+    # try block does not raise any exceptions
+    try:
+        print("In doNotRaiseException()")
+
+    # finally executes because corresponding try executed
+    finally:
+        print("Finally executed in doNotRaiseException")
+
+    print("End of doNotRaiseException")
+
+def raiseExceptionDoNotCatch():
+    # raise exception, but do not catch it
+    try:
+        print("In raiseExceptionDoNotCatch()")
+        raise Exception
+    # finally executes because corresponding try executed
+    finally:
+        print("Finally executed in raiseExceptionDoNotCatch")
+    
+    print("Will never reach this point")
+
+# main program
+# case 1: no exceptions occurs in called function
+doNotRaiseException()
+
+# case2: Exception occurs, but is not handled in called function,
+# because no except clauses exist in raiseExceptionDoNotCatch
+
+try:
+    raiseExceptionDoNotCatch()
+
+except Exception:
+    print("Caught exception from raiseExceptionDoNotCatch")
