c3a1be2bc95f2e2c27e764304f75750cfd2edf1c
wesinalves <wesinec@gmail.com>
2019-09-13 16:14:25 -0300

    writing in a file using pickle module

---
+++ codigos/cap13/pickle_writing.py
@@ -0,0 +1,29 @@
+# Python Journey - Chapter 13
+# Opening and writing pickled object to file.
+
+import sys, pickle
+
+# open file
+try:
+    file = open("users.dat", "wb")
+except IOError:
+    print("File not found", file = sys.stderr)
+    sys.exit(1)
+
+print("Enter the user name, name and date of birth.")
+print("Enter end-of-file to end input.")
+
+input_list = []
+
+while 1:
+
+    try:
+        account_line = input("? ")
+    except EOFError:
+        break
+    else:
+        input_list.append(account_line.split())
+
+pickle.dump(input_list, file)
+
+#file.close()
