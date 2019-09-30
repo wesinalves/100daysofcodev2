6aef758e7b663fe5c9dc37a5af52e7145e651623
wesinalves <wesinec@gmail.com>
2019-09-30 16:45:32 -0300

    update crud example

--- codigos/cap14/crud.py
+++ codigos/cap14/crud.py
@@ -27,7 +27,7 @@ class Film(Frame):
         # list of fields in an address record
         fields = ["film_id", "title", "description", "release_year", 
                     "lenguage_id", "rental_duration", "rental_rate", "length",
-                    "replacement_cost", "rating", "last_updatd", "special_features", "full_text"]
+                    "replacement_cost", "rating", "last_updated", "special_features", "full_text"]
         
         # dictionary with Entry components for values, keyed by
         # corresponding addresses table field names
@@ -50,4 +50,42 @@ class Film(Frame):
             key = fields[i].replace(" ", " ")
             key = key.upper()
             self.entries[key] = entry
+        
+    def add_film(self):
+        """Add film record to database"""
+
+        if self.entries["title"].get() != " " and self.entries["description"].get() != " ":
+            # create INSERT query command
+            query = """INSERT INTO film (title, description, release_year, 
+                    lenguage_id, rental_duration, rental_rate, length,
+                    replacement_cost, rating, last_updated, special_features, full_text)
+                    VALUES (""" + "'%s', " * 10 % \
+                    (
+                        self.entries["title"].get(),
+                        self.entries["description"].get(),
+                        self.entries["release_year"].get(),
+                        self.entries["lenguage_id"].get(),
+                        self.entries["rental_duration"].get(),
+                        self.entries["rental_rate"].get(),
+                        self.entries["length"].get(),
+                        self.entries["replacement_cost"].get(),
+                        self.entries["rating"].get(),
+                        self.entries["last_updated"].get(),
+                        self.entries["especial_features"].get(),
+                        self.entries["full_text"].get()
+                    )
+            query = query[:-2] + ")" 
+
+            # open connection, retrieve cursor and execute query
+            try:
+                conn = psycopg2.connect("dbname=dvdrental user=postgres password=root")
+                cursor = conn.cursor()
+                cursor.execute(query)
+            except psycopg2.OperationalError as error:
+                error_message = "Error %d: \n%s" % (error[0], error[1])
+                showerror("Error", error_message)
+            else:
+                cursor.close()
+                conn.close()
+                self.clear_contents()
             
