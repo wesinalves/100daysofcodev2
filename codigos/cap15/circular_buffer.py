c433a2b11c47fc3bda0902dee0c297993bb198ca
wesinalves <wesinec@gmail.com>
2019-10-09 18:01:37 -0300

    add circular buffer example

---
+++ codigos/cap15/circular_buffer.py
@@ -0,0 +1,22 @@
+# Python Journey - Chapter 15
+# Show multiple threads modifying shared object
+
+from SynchronizedCells import SynchronizedCells
+from ProduceInteger import ProduceInteger
+from ConsumeInteger import ConsumeInteger
+
+# initialize integer and threads
+number = SynchronizedCells()
+producer = ProduceInteger("Producer", number)
+consumer = ConsumeInteger("Consumer", number)
+
+print("Start threads...\n")
+
+producer.start()
+consumer.start()
+
+# wait for threads to terminate
+producer.join()
+consumer.join()
+
+print("\nAll threads have terminated.")
