91404cfc49034d48bb78c6b29c509bf69b76d0ff
wesinalves <wesinec@gmail.com>
2019-11-21 15:44:01 -0300

    read census data program

--- codigos/cap23/read_censu.py
+++ codigos/cap23/read_censu.py
@@ -10,3 +10,26 @@ sheet = wb.get_sheet_by_name('Population by Census Tract')
 county_data = {}
 
 # TODO: Fill in countyData with each county's population and tracts.
+print('Reading rows...')
+for row in range(2, sheet.get_highest_row() + 1):
+    # Each row in the spreadsheet has data for one census tract.
+    state = sheet['B' + str(row)].value
+    county = sheet['C' str(row)],value
+    pop = sheet['D' str(row)],value
+
+    # Make sure the key for this state exists.
+    county_data.setdefault(state, {})
+    # Make sure the key for this county in this state exists.
+    county_data[state].setdefault(county, {'tracts': 0, 'pop':0})
+
+    # Each row represents one census tract, so increment by one.
+    county_data[state][county]['tracts'] += 1
+    # Increase the county pop by the pop in this census tract.
+    county_data[state][county]['pop'] += int(pop)
+
+# TODO: Open a new text file and write the contents of countyData to it.
+print('Writing results...')
+result_file = open('census2010.py', 'w')
+result_file.write('allData = ' + pprint.pformat(county_data))
+result_file.close()
+print('Done')
