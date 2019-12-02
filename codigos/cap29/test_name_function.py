f201ed1cd4fea041b907c0231c5ba9683e10d6bd
wesinalves <wesinec@gmail.com>
2019-12-02 16:09:50 -0300

    unittest in name function file

---
+++ codigos/cap29/test_name_function.py
@@ -0,0 +1,15 @@
+# Python Journey - Chapter 19
+# Unit test for name function
+
+import unittest
+from name_function import get_formatted_name
+
+class NamesTestCase(unittest.TestCase):
+    """Tests for 'name_function.py'."""
+
+    def test_first_last_name(self):
+        """Do names like 'Tony Stark' work?"""
+        formatted_name = get_formatted_name('tony', 'stark')
+        self.assertEqual(formatted_name, 'Tony Stark')
+
+unittest.main()
