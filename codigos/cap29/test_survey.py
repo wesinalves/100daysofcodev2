7c446ec42dbcf47f311e9cbf90626e0c23976159
wesinalves <wesinec@gmail.com>
2019-12-04 15:56:04 -0300

    update test_survey.py

--- codigos/cap29/test_survey.py
+++ codigos/cap29/test_survey.py
@@ -13,5 +13,18 @@ class TestAnonymousSurvey(unittest.TestCase):
         my_survey = AnonymousSurvey(question)
         my_survey.store_response('English')
         self.assertIn('English', my_survey.responses)
+    
+    def test_store_three_responses(self):
+        """Test that three individual responses are stored properly."""
+        question = "What language did you first learn to speak?"
+        my_survey = AnonymousSurvey(question)
+        responses = ['English', 'Spanish', 'Mandarin']
+        for response in responses:
+            my_survey.store_response(response)
+        
+        for response in responses:
+            self.assertIn(response, my_survey.responses)
+        
+
 
 unittest.main()
