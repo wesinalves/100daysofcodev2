81c5abd1409603d5a991afd8ba1570a622f03486
wesinalves <wesinec@gmail.com>
2019-12-05 15:23:30 -0300

    alien invasion game

---
+++ codigos/cap30/alien_invasion.py
@@ -0,0 +1,29 @@
+# Python Journey - Chapter 29
+# Creating a Pygame Window and Responding to User Input for Alien Invasion project
+
+import sys
+import pygame
+
+def run_game():
+    # Initialize game and create a screen object.
+    pygame.init()
+    screen = pygame.display.set_mode((1200,700))
+    pygame.display.set_caption("Alien Invasion")
+
+    # set the background color
+    bg_color = (230,230,230)
+
+    # Start the main loop for the game.
+    while True:
+
+        # Watch for keyboard and mouse events.
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                sys.exit()
+        
+        # Redraw the screen during each pass through the loop.
+        screen.fill(bg_color)
+        # Make the most recently drawn screen visible.
+        pygame.display.flip()
+
+run_game()
