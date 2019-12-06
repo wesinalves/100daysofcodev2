037324eef2e58d0aeb03a2cba06e3fafe3dc7fef
wesinalves <wesinec@gmail.com>
2019-12-06 14:57:36 -0300

    upadate alien invasion

--- codigos/cap30/alien_invasion.py
+++ codigos/cap30/alien_invasion.py
@@ -4,14 +4,17 @@
 import sys
 import pygame
 
+from settings import Settings
+
 def run_game():
     # Initialize game and create a screen object.
     pygame.init()
-    screen = pygame.display.set_mode((1200,700))
+    ai_settings = Settings()
+    screen = pygame.display.set_mode((ai_settings.screen_width, ai_settings.screen_height))
     pygame.display.set_caption("Alien Invasion")
 
     # set the background color
-    bg_color = (230,230,230)
+    bg_color = ai_settings.bg_color
 
     # Start the main loop for the game.
     while True:
