d143078a955ab62ed56b7375bb927aa5f8d7cd89
wesinalves <wesinec@gmail.com>
2019-12-09 16:04:40 -0300

    add a ship in alien invasion screen

--- codigos/cap30/alien_invasion.py
+++ codigos/cap30/alien_invasion.py
@@ -5,6 +5,7 @@ import sys
 import pygame
 
 from settings import Settings
+from ship import Ship
 
 def run_game():
     # Initialize game and create a screen object.
@@ -16,6 +17,9 @@ def run_game():
     # set the background color
     bg_color = ai_settings.bg_color
 
+    # Make a ship.
+    ship = Ship(screen)
+
     # Start the main loop for the game.
     while True:
 
@@ -26,6 +30,8 @@ def run_game():
         
         # Redraw the screen during each pass through the loop.
         screen.fill(bg_color)
+        ship.blitme()
+        
         # Make the most recently drawn screen visible.
         pygame.display.flip()
 
