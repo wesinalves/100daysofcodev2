0e84310dc0358f417af0f0e2a8b10bb1eb4b7c2d
wesinalves <wesinec@gmail.com>
2019-12-10 17:22:51 -0300

    move ship to the left

--- codigos/cap30/game_functions.py
+++ codigos/cap30/game_functions.py
@@ -13,10 +13,16 @@ def check_events(ship):
             if event.key == pygame.K_RIGHT:
                 # Move the ship to the right
                 ship.moving_right = True
+            elif event.key == pygame.K_LEFT:
+                # Move the ship to the right
+                ship.moving_left = True
         elif event.type == pygame.KEYUP:
             if event.key == pygame.K_RIGHT:
                 # Stop moving the ship to the right
                 ship.moving_right = False
+            elif event.key == pygame.K_LEFT:
+                # Stop moving the ship to the right
+                ship.moving_left = False
             
                     
 
--- codigos/cap30/ship.py
+++ codigos/cap30/ship.py
@@ -21,11 +21,14 @@ class Ship:
 
         # Movement flag
         self.moving_right = False
+        self.moving_left = False
     
     def update(self):
         """Update the ship's position based on the movement flag."""
         if self.moving_right:
             self.rect.centerx += 1
+        if self.moving_left:
+            self.rect.centerx -= 1
     
     def blitme(self):
         
