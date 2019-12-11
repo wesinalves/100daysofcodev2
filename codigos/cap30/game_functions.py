f1c9e348c4be29ef23763b16325843fc56229c44
wesinalves <wesinec@gmail.com>
2019-12-11 15:43:11 -0300

    updating game functions

--- codigos/cap30/game_functions.py
+++ codigos/cap30/game_functions.py
@@ -4,25 +4,34 @@
 import sys
 import pygame
 
+def check_keydown_events(event, ship):
+    """Respond to keypresses."""
+    if event.key == pygame.K_RIGHT:
+        # Move the ship to the right
+        ship.moving_right = True
+    elif event.key == pygame.K_LEFT:
+        # Move the ship to the right
+        ship.moving_left = True
+
+def check_keyup_events(event, ship):
+    """Respond to keyreleases."""
+    if event.key == pygame.K_RIGHT:
+        # Stop moving the ship to the right
+        ship.moving_right = False
+    elif event.key == pygame.K_LEFT:
+        # Stop moving the ship to the right
+        ship.moving_left = False
+
 def check_events(ship):
     """Respond to keypresses and mouse events."""
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             sys.exit()
         elif event.type == pygame.KEYDOWN:
-            if event.key == pygame.K_RIGHT:
-                # Move the ship to the right
-                ship.moving_right = True
-            elif event.key == pygame.K_LEFT:
-                # Move the ship to the right
-                ship.moving_left = True
+            check_keydown_events(event, ship)
         elif event.type == pygame.KEYUP:
-            if event.key == pygame.K_RIGHT:
-                # Stop moving the ship to the right
-                ship.moving_right = False
-            elif event.key == pygame.K_LEFT:
-                # Stop moving the ship to the right
-                ship.moving_left = False
+            check_keyup_events(event, ship)
+            
             
                     
 
