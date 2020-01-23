6db67dc982b672d4cedc31a7e0e7db3979ae06f9
wesin ribeiro <wesinec@gmail.com>
2020-01-23 16:35:10 -0300

    add entry model

--- codigos/cap33/learning_logs/models.py
+++ codigos/cap33/learning_logs/models.py
@@ -10,3 +10,16 @@ class Topic(models.Model):
     def __str__(self):
         """Return a string representation of the model."""
         return self.text
+
+class Entry(models.Model):
+    """Something specific learned about a topic"""
+    topic = models.ForeignKey(Topic, on_delete = models.PROTECT)
+    text = models.TextField()
+    date_added = models.DateTimeField(auto_now_add = True)
+
+    class Meta:
+        verbose_name_plural = 'entries'
+    
+    def __str__(self):
+        """Return a string representation of the model."""
+        return self.text[:50] + "..."
