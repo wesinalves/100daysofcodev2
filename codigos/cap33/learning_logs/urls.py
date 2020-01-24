2fb7425a2ab0012f608ff68d561104c6ad0829cb
wesin ribeiro <wesinec@gmail.com>
2020-01-24 16:09:31 -0300

    add topics page

--- codigos/cap33/learning_logs/urls.py
+++ codigos/cap33/learning_logs/urls.py
@@ -6,5 +6,8 @@ app_name = 'learning_logs'
 
 urlpatterns = [
     # Home
-    url(r'^$', views.index, name='index'),
+    url(r'^$', views.index, name = 'index'),
+
+    # Show all topics
+    url(r'^topics/$', views.topics, name = 'topics'),
 ]
