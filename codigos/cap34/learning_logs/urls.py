6ad1dcc9c3f98df0ced35a4bf27df42b130ec53f
wesin ribeiro <wesinec@gmail.com>
2020-01-30 16:11:39 -0300

    add topic form

---
+++ codigos/cap34/learning_logs/urls.py
@@ -0,0 +1,19 @@
+"""Defines URL patterns for learning_logs."""
+from django.conf.urls import url
+from . import views
+
+app_name = 'learning_logs'
+
+urlpatterns = [
+    # Home
+    url(r'^$', views.index, name='index'),
+
+    # Show all topics
+    url(r'^topics/$', views.topics, name='topics'),
+
+    # Detail page for a single topic
+    url(r'^topics/(?P<topic_id>\d+)/$', views.topic, name='topic'),
+
+    # Page for adding a new topic
+    url(r'^new_topic/$', views.new_topic, name='new_topic'),
+]
---
+++ codigos/cap34/learning_logs/views.py
@@ -0,0 +1,39 @@
+from django.shortcuts import render
+from django.http import HttpResponseRedirect
+from django.urls import reverse
+
+from .models import Topic
+from .forms import TopicForm 
+
+# Create your views here.
+
+def index(request):
+    """The home page for Learning Log"""
+    return render(request, 'learning_logs/index.html')
+
+def topics(request):
+    """Show all topics."""
+    topics = Topic.objects.order_by('date_added')
+    context = {'topics': topics}
+    return render(request, 'learning_logs/topics.html', context)
+
+def topic(request, topic_id):
+    """Show a single page topic and all its entries"""
+    topic = Topic.objects.get(id=topic_id)
+    entries = topic.entry_set.order_by('-date_added')
+    context = {'topic': topic, 'entries': entries}
+    return render(request, 'learning_logs/topic.html', context)
+
+def new_topic(request):
+    """Add a new topic"""
+    if request.method != 'POST':
+        # no data submitted; create a blank form
+        form = TopicForm()
+    else:
+        # POST data submitted; process the form
+        form = TopicForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return HttpResponseRedirect(reverse('learning_logs:topics'))
+    context = {'form': form}
+    return render(request, 'learning_logs/new_topic.html', context)
