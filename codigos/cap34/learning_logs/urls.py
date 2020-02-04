b7ba9771a51bacb98e887a54edf08f8eb72e0a5b
wesin ribeiro <wesinec@gmail.com>
2020-02-04 16:36:23 -0300

    edit entry form

--- codigos/cap34/learning_logs/urls.py
+++ codigos/cap34/learning_logs/urls.py
@@ -19,4 +19,8 @@ urlpatterns = [
 
     # Page for adding a new entry
     url(r'^new_entry/(?P<topic_id>\d+)/$', views.new_entry, name='new_entry'),
+
+    # Page for editing an entry
+    url(r'^edit_entry/(?P<entry_id>\d+)/$', views.edit_entry, name='edit_entry'),
+
 ]
