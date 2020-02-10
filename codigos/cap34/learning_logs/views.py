cfa7712b0e852e7a619e3cd5768049ec977a4697
wesin ribeiro <wesinec@gmail.com>
2020-02-10 15:30:09 -0300

    restricting pages

--- codigos/cap34/learning_logs/views.py
+++ codigos/cap34/learning_logs/views.py
@@ -3,20 +3,22 @@ from django.http import HttpResponseRedirect
 from django.urls import reverse
 
 from .models import Topic, Entry
-from .forms import TopicForm, EntryForm 
-
+from .forms import TopicForm, EntryForm
+from django.contrib.auth.decorators import login_required
 # Create your views here.
 
 def index(request):
     """The home page for Learning Log"""
     return render(request, 'learning_logs/index.html')
 
+@login_required
 def topics(request):
     """Show all topics."""
     topics = Topic.objects.order_by('date_added')
     context = {'topics': topics}
     return render(request, 'learning_logs/topics.html', context)
 
+@login_required
 def topic(request, topic_id):
     """Show a single page topic and all its entries"""
     topic = Topic.objects.get(id=topic_id)
@@ -24,6 +26,7 @@ def topic(request, topic_id):
     context = {'topic': topic, 'entries': entries}
     return render(request, 'learning_logs/topic.html', context)
 
+@login_required
 def new_topic(request):
     """Add a new topic"""
     if request.method != 'POST':
@@ -38,6 +41,7 @@ def new_topic(request):
     context = {'form': form}
     return render(request, 'learning_logs/new_topic.html', context)
 
+@login_required
 def new_entry(request, topic_id):
     """Add a new entry for a particular topic"""
     topic = Topic.objects.get(id=topic_id)
@@ -56,6 +60,7 @@ def new_entry(request, topic_id):
     context = {'topic': topic, 'form': form}
     return render(request, 'learning_logs/new_entry.html', context)
 
+@login_required
 def edit_entry(request, entry_id):
     """Edit an existing entry"""
     entry = Entry.objects.get(id=entry_id)
