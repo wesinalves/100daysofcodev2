620d51f420bc777447a56f7163856f6d74165023
wesin ribeiro <wesinec@gmail.com>
2020-02-13 11:12:10 -0300

    styling pages

--- codigos/cap35/learning_log/settings.py
+++ codigos/cap35/learning_log/settings.py
@@ -23,9 +23,9 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = '(j3u^4x!=dp5sx$n*gimu97take051+20*tz$um=d57u)(vxd%'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['127.0.0.1']
 
 
 # Application definition
