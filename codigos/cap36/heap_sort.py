2dc99115b7324e65b2ee387a6fbd7d99adf5c69f
wesin ribeiro <wesinec@gmail.com>
2020-02-17 16:31:01 -0300

    updating heap sort

--- codigos/cap36/heap_sort.py
+++ codigos/cap36/heap_sort.py
@@ -5,40 +5,35 @@
 GOAL_SCORES = [16, 15, 7, 6, 8, 10, 9, 12, 13, 11]
 
 # processing
-def max_heapfy(vector, n, i):
-    """Create max binary tree"""
-    current_parent = i
-    child_left = 2 * i + 1
-    child_right = 2 * i + 2
+def max_heapfy(elements, length, index):
+    """Create a max binary tree"""
+    current_parent = index
+    child_left = index * 2 + 1
+    child_right = index * 2 + 2
 
-    ## see if the left child exists and if is grater than current parent
-    if child_left < n and vector[child_left] > vector[current_parent]:
+    # check if child_left value is lower than current_parent value
+    if child_left < length and elements[child_left] > elements[current_parent]:
         current_parent = child_left
-    ## see if the right child exists and if is grater than current parent
-    if child_right < n and vector[child_right] > vector[current_parent]:
+    # check if child_rigth value is lower than current_parent value
+    if child_right < length and elements[child_right] > elements[current_parent]:
         current_parent = child_right
-    # change root if needed
-    if current_parent != i:
-        # current parent is now a index of one of his children
-        vector[i], vector[current_parent] = vector[current_parent], vector[i]
-        max_heapfy(vector, n, current_parent)           
-def heap_sort(vector):
-    """Sort array in ascending order"""
-
-    n = len(vector)
-
+    # check if current_parent was changed to swap values
+    if current_parent != index:
+        elements[index], elements[current_parent] = elements[current_parent], elements[index]
+        max_heapfy(elements, length, current_parent)
+def build_max_heap(elements):
+    """Sort an element list in ascending order"""
+    length = len(elements)
     # build a max heap
-    for i in range(n//2, 0, -1):
-        max_heapfy(vector, n, i)
-    # sort elements
-    for i in range(n-1, 0, -1):
-        print(vector)
-        vector[i], vector[0] = vector[0], vector[i]
-        max_heapfy(vector, i, 0)
+    for i in range(length//2, 0, -1):
+        max_heapfy(elements, length, i)
+    # print sorted elements
+    for i in range(length - 1, 0, -1):
+        print(elements)
+        elements[i], elements[0] = elements[0], elements[i]
+        max_heapfy(elements, i, 0)
 
 
 # termination
-
 print(GOAL_SCORES)
-heap_sort(GOAL_SCORES)
-#print(GOAL_SCORES)
+build_max_heap(GOAL_SCORES)
