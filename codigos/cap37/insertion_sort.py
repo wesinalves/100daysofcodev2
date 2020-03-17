4fe28ed64cd5a0cf65a90bceae3a70bbd38d0172
wesin ribeiro <wesinec@gmail.com>
2020-03-17 15:50:47 -0300

    add insertion sort

---
+++ codigos/cap37/insertion_sort.py
@@ -0,0 +1,25 @@
+# Journey Python - Chapter 37
+# Quick sort implementation.
+from random import shuffle
+
+# initialization
+GOAL_SCORES = list(range(15))
+shuffle(GOAL_SCORES)
+
+# process
+def insertion_sort(elements):
+    """Order elements using insertion sort algorithm"""
+    length = len(elements)
+    for j in range(1, length):
+        key = elements[j]
+        # put the key in the right position in A[1..j-1]
+        i = j - 1
+        while i > -1 and elements[i] > key:
+            elements[i+1] = elements[i]
+            i -= 1
+        elements[i+1] = key
+        print(elements)
+
+# termination
+print(GOAL_SCORES)
+insertion_sort(GOAL_SCORES)
