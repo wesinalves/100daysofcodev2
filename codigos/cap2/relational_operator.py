a8c7b9b73f54b591eeebc8b8b66c5d2ada107c8d
wesin ribeiro <wesinec@gmail.com>
2020-03-23 19:11:41 -0300

    add exercises

--- codigos/cap2/relational_operator.py
+++ codigos/cap2/relational_operator.py
@@ -5,27 +5,27 @@ print("Enter two integers, and I will tell you")
 print("the relationships they satisfy.")
 
 # read first string and convert to integer
-number1 = input( "Please enter first integer: " )
-number1 = int( number1 )
+number1 = input("Please enter first integer: ")
+number1 = int(number1)
 
 # read second string and convert to integer
-number2 = input( "Please enter second integer: " )
-number2 = int( number2 )
+number2 = input("Please enter second integer: ")
+number2 = int(number2)
 
 if number1 == number2:
-    print("%d is equal to %d" % ( number1, number2 ))
+    print("%d is equal to %d" % (number1, number2))
 
 if number1 != number2:
-    print("%d is not equal to %d" % ( number1, number2 ))
+    print("%d is not equal to %d" % (number1, number2))
 
 if number1 < number2:
-    print("%d is less than %d" % ( number1, number2 ))
+    print("%d is less than %d" % (number1, number2))
 
 if number1 > number2:
-    print("%d is greater than %d" % ( number1, number2 ))
+    print("%d is greater than %d" % (number1, number2))
 
 if number1 <= number2:
-    print("%d is less than or equal to %d" % ( number1, number2 ))
+    print("%d is less than or equal to %d" % (number1, number2))
 
 if number1 >= number2:
-    print("%d is greater than or equal to %d" % ( number1, number2 ))
+    print("%d is greater than or equal to %d" % (number1, number2))
