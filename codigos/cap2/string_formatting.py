a8370476a038e6511cd8cf9fec8af0243d527d64
WesinAlves <wesinec@gmail.com>
2020-04-13 22:46:58 -0300

    constants in uppercase

--- codigos/cap2/string_formatting.py
+++ codigos/cap2/string_formatting.py
@@ -6,20 +6,20 @@ print("O Senhor é meu pastor, nada me faltará. \"Salmo 23:1\"")
 print('O Senhor é meu pastor, nada me faltará. \'Salmo 23:1\'')
 print('O Senhor é meu pastor, nada me faltará. "Salmo 23:1"')
 
-integerValue = 4237.05
-print("Integer", integerValue)
-print("decimal Integer {:.0f}".format(integerValue))
-print("Hexadecimal integer {:x}\n".format(int(integerValue)))
-print("Right justify Integer ({:8d})".format(int(integerValue)))
-print("Left justify integer (%-8d)\n" % integerValue)
+INTEGER_VALUE = 4237.05
+print("Integer", INTEGER_VALUE)
+print("decimal Integer {:.0f}".format(INTEGER_VALUE))
+print("Hexadecimal integer {:x}\n".format(int(INTEGER_VALUE)))
+print("Right justify Integer ({:8d})".format(int(INTEGER_VALUE)))
+print("Left justify integer (%-8d)\n" % INTEGER_VALUE)
 
-floatValue = 4237.05
-print("float", floatValue)
-print("default float {:f}".format(floatValue))
-print("defual exponential float {:e}\n".format(int(floatValue)))
+FLOAT_VALUE = 4237.05
+print("float", FLOAT_VALUE)
+print("default float {:f}".format(FLOAT_VALUE))
+print("defual exponential float {:e}\n".format(int(FLOAT_VALUE)))
 
-stringValue = "String formatting"
-print("Force eight digits in integer %.8d" % integerValue)
-print("Five digits after decimal in float %.5f" % floatValue)
+STRING_VALUE = "String formatting"
+print("Force eight digits in integer %.8d" % INTEGER_VALUE)
+print("Five digits after decimal in float %.5f" % FLOAT_VALUE)
 print("Fifteen and five characters allowed in string:")
-print("({:15s}) ({:.5s})".format(stringValue, stringValue))
+print("({:15s}) ({:.5s})".format(STRING_VALUE, STRING_VALUE))
