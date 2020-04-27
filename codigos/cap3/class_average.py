41d137535bb39b37a3084400249157e83755b484
WesinAlves <wesinec@gmail.com>
2020-04-27 16:46:31 -0300

    add pseudo code

--- codigos/cap3/class_average.py
+++ codigos/cap3/class_average.py
@@ -1,7 +1,21 @@
 '''
 How to program in Python - Chapter 3
 Class average problem with counter-controlled repetition.
+#########################################################
+A class of ten students took a quiz. The grades (integers numbers) for this quiz
+are given by keyboard. Determine the class average on the quiz.
 '''
+
+# Set total to zero
+# Set grade counter to one
+# While grade counter is less than or equal to ten
+#   Input the next grade
+#   Add the grade into the total
+#   Add one to the grade counter
+# Set the class average to the total divided by ten
+# Print the class average
+
+
 #initialization
 TOTAL = 0
 GRADE_COUNTER = 0
