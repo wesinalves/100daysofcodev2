fb942932f246ae1b0411b4509809be74a82e7ca9
WesinAlves <wesinec@gmail.com>
2020-04-28 10:18:37 -0300

    add small changes

--- codigos/cap3/class_average.py
+++ codigos/cap3/class_average.py
@@ -6,18 +6,17 @@ A class of ten students took a quiz. The grades (integers numbers) for this quiz
 are given by keyboard. Determine the class average on the quiz.
 '''
 
-#initialization
+# initialization
 TOTAL = 0
 GRADE_COUNTER = 1
 
-#processing
+# processing
 while GRADE_COUNTER <= 10:
     GRADE = input("Enter GRADE: ")
     GRADE = int(GRADE)
     TOTAL = TOTAL + GRADE
     GRADE_COUNTER = GRADE_COUNTER + 1
 
-#termination
+# termination
 AVERAGE = TOTAL / 10
 print("Class AVERAGE is :", AVERAGE)
-
--- codigos/cap3/pseudo_class_average.py
+++ codigos/cap3/pseudo_class_average.py
@@ -1,11 +1,5 @@
-# Atribuir zero a total
-# atribuir um a grade counter
-# Enquanto contador de nota é menor ou igual a 10
-#   Entre com a próxima nota
-#   adicione nota a total
-#   adicione um ao contador de nota
-# atribua o total dividido por dez a média da classe
-# imprima a média da classe
+# A class of ten students took a quiz. The grades (integers numbers) for this quiz
+# are given by keyboard. Determine the class average on the quiz.
 
 # Set total to zero
 # Set grade counter to one
@@ -15,3 +9,12 @@
 #   Add one to the grade counter
 # Set the class average to the total divided by ten
 # Print the class average
+
+# Atribuir zero a total
+# atribuir um a grade counter
+# Enquanto contador de nota é menor ou igual a 10
+#   Entre com a próxima nota
+#   adicione nota a total
+#   adicione um ao contador de nota
+# atribua o total dividido por dez a média da classe
+# imprima a média da classe
