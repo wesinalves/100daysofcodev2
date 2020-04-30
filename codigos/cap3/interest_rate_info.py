348ae440ed9a198f5390c370dced514082cb3813
WesinAlves <wesinec@gmail.com>
2020-04-30 14:45:14 -0300

    small changes

--- codigos/cap3/interest_rate_info.py
+++ codigos/cap3/interest_rate_info.py
@@ -1,12 +1,8 @@
-# A person invests $1000 in a savings account yielding 5 percent interest. Assuming that all
-# interest is left on deposit in the account, calculate and print the amount of money in the
-# account at the end of each year for 10 years. Use the following formula for determining
-# these amounts:
-
 # a = p * ( 1 + r ) ** n
 
 # where
-# p is the original amount invested (i.e., the principal),
+# p is the original amount invested,
 # r is the annual interest rate,
 # n is the number of years and
-# a is the amount on deposit at the end of the nth year.
+# a is the amount on deposit 
+# at the end of the nth year.
