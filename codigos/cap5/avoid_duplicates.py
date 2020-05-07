dd05fe207d47d60f2ca362932f9f71f1e169f606
WesinAlves <wesinec@gmail.com>
2020-05-07 15:41:11 -0300

    set to avoid duplicates

---
+++ codigos/cap5/avoid_duplicates.py
@@ -0,0 +1,17 @@
+'''
+How to program in Python - Chapter 5
+Avoiding duplicated words.
+'''
+
+def main():
+    '''Main program'''
+    fruits = set()
+    fruit = input('Enter a fruit (blank line to exit)')
+    while fruit != '':
+        if fruit not in fruits:
+            fruits.add(fruit)
+        fruit = input('Enter a fruit (blank line to exit)')
+    for item in fruits:
+        print(item, end=',')
+    
+main()
