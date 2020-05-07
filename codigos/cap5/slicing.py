e97c4d5b34dfa7e47999a02d35efdb8db527ff59
WesinAlves <wesinec@gmail.com>
2020-05-07 14:29:32 -0300

    slicing sequences

---
+++ codigos/cap5/slicing.py
@@ -0,0 +1,33 @@
+'''
+How to program in Python - Chapter 5
+Slicing sequences.
+'''
+
+
+def slicing():
+    '''Slicing sequences example'''
+    # create sequences
+    slice_string = "abcdefghij"
+    slice_tuple = (1, 2, 3, 4, 5, 6, 7, 8, 9, 10)
+    slice_list = ["I", "II", "III", "IV", "V", \
+        "VI", "VII", "VIII", "IX", "X"]
+
+    # print strings
+    print("slice_string: ", slice_string)
+    print("slice_tuple: ", slice_tuple)
+    print("slice_list: ", slice_list)
+    print()
+
+    # get slices
+    start = int(input("Enter start: "))
+    end = int(input("Enter end: "))
+
+    # print slices
+    print("\nslice_string[", start, ":", end, "] = ",
+          slice_string[start:end])
+    print("\nslice_tuple[", start, ":", end, "] = ",
+          slice_tuple[start:end])
+    print("\nslice_list[", start, ":", end, "] = ",
+          slice_list[start:end])
+
+slicing()
