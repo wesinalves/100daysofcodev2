5b8f4852b138222d107ed790ac2616a082cb740e
WesinAlves <wesinec@gmail.com>
2020-05-07 14:30:33 -0300

    homogeneous list

--- codigos/cap5/homogeneous_list.py
+++ codigos/cap5/homogeneous_list.py
@@ -3,32 +3,36 @@ How to program in Python - Chapter 5
 Creating, accessing and changing a list.
 '''
 
-MY_LIST = []  # create empty list
+def create_list():
+    '''Creating list'''
+    my_list = []  # create empty list
 
-# add values to list
-for number in range(1, 11):
-    MY_LIST += [number]
+    # add values to list
+    for number in range(1, 11):
+        my_list += [number]
 
-print("The value of MY_LIST is:", MY_LIST)
+    print("The value of my_list is:", my_list)
 
-# access list values by iteration
-print("\nAccessing values by iteration:")
+    # access list values by iteration
+    print("\nAccessing values by iteration:")
 
-for item in MY_LIST:
-    print(item, end="\t")
+    for item in my_list:
+        print(item, end="\t")
 
-print()
+    print()
 
-# access list values by index
-print("\nAccessing values by index:")
-print("Subscript Value")
+    # access list values by index
+    print("\nAccessing values by index:")
+    print("Subscript Value")
 
-for index, value in enumerate(MY_LIST):
-    print("%9d %7d" % (index, value))
+    for index, value in enumerate(my_list):
+        print("%9d %7d" % (index, value))
 
-# modify list
-print("\nModifying a list value...")
-print("Value of MY_LIST before modification:", MY_LIST)
-MY_LIST[0] = -100
-MY_LIST[-3] = 19
-print("Value of MY_LIST after modification:", MY_LIST)
+    # modify list
+    print("\nModifying a list value...")
+    print("Value of my_list before modification:", my_list)
+    my_list[0] = -100
+    my_list[-3] = 19
+    print("Value of my_list after modification:", my_list)
+
+create_list()
