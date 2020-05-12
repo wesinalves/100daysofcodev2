cf8dc796bd92dfdc6d1fcb23ba71b70673e9fc48
WesinAlves <wesinec@gmail.com>
2020-05-12 13:53:24 -0300

    pick class

---
+++ codigos/cap6/pick.py
@@ -0,0 +1,21 @@
+'''
+How to program in Python - chapter 6
+Class attribute example using class Pick
+'''
+
+class Pick:
+    '''Represents a pick'''
+    count = 0  # class attribute
+
+    def __init__(self, size=0.5, color='red'):
+        self.size = size
+        self.color = color
+
+        Pick.count += 1
+
+        print('Pick{} with size {} and color {}'.format(self.count, self.size, self.color))
+
+    def __del__(self):
+        '''Decrements count and print messages'''
+        Pick.count -= 1
+        print('Decrementing pick counter using destructor')
