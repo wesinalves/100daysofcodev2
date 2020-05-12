958353a7b98128f4d26646a9bb1586ed6347a0df
WesinAlves <wesinec@gmail.com>
2020-05-12 13:53:17 -0300

    guitar class

--- codigos/cap6/guitar.py
+++ codigos/cap6/guitar.py
@@ -1,24 +1,34 @@
+'''
+How to program in Python - Chapter 6
+Simple definition of class Guitar.
+'''
+
 class Guitar:
+    '''class Guitar definition'''
+
     def __init__(self):
-        self._brand = 'Taylor'
-        self._body = 'solid'
-        self._shape = 'grand concert'
-        self._numStrings = 6
-        self._model = '314ce'
-        self._strings = []
-        self._price = 2000
+        self.brand = 'Taylor'
+        self.body = 'solid'
+        self.shape = 'grand concert'
+        self.num_strings = 6
+        self.model = '314ce'
+        self.strings = []
+        self.price = 2000
 
     def play(self, style, bpm, music):
-        return "Let's play {} at {} bpm using {} style with {} {}".format(music, bpm, style, self._brand, self._model)
-    
-    def tuneStrings(self, afination=['E','A','D','G','B','e']):
-        self._strings = afination
-    
-    def checkStrings(self):
-        if len(self._strings) == 0:
+        '''Play song'''
+        return "Let's play {} at {} bpm using {} style with {} {}".format(music, bpm, style,
+                                                                          self.brand, self.model)
+
+    def tune_strings(self, afination=None):
+        '''Tuning strings'''
+        if afination is None:
+            afination = ['E', 'A', 'D', 'G', 'B', 'e']
+        self.strings = afination
+
+    def check_strings(self):
+        '''Cheking strings tuning'''
+        if len(self.strings) == 0:
             print("Guitar is not tuned!")
         else:
-            print(self._strings)
-    
-    def equalize(self):
-        pass
+            print(self.strings)
