9157495c9ded90aeea889cbe2087a07031899f9b
WesinAlves <wesinec@gmail.com>
2020-05-12 13:53:50 -0300

    string example

--- codigos/cap6/string.py
+++ codigos/cap6/string.py
@@ -1,15 +1,38 @@
-# Jornada Python - Capítulo 6
-# String class.
+'''
+Jornada Python - Capítulo 6
+Set and Get methods of class String.
+'''
+
+STRINGS = {'E':82, 'A':110, 'D':146, 'G':196, 'B':247, 'e':330}
 
 class String:
-    """Simulate a string of guitar"""
+    '''Simulate a string of guitar'''
 
     def __init__(self):
-        self.name = "A"
-        self.frequency = 440
-    
+        self._name = "A"
+        self._frequency = 110
+        self.__type = 'Nylon'
     def set_name(self, name):
-        self.name = name
-
+        '''Set name attribute '''
+        if name in STRINGS:
+            self._name = name
     def set_frequency(self, value):
-        self.frequency = value
+        '''Set frequency attribute'''
+        if 82 <= value < 330:
+            self._frequency = value
+    def get_name(self):
+        '''Get name attribute'''
+        return self._name + ' string'
+    def get_frequency(self):
+        '''Get freqeuncy attribute'''
+        return str(self._frequency / 1000) + ' Khz' # return value in KHz
+    def get_type(self):
+        '''Get type attribute'''
+        return self.__type
+    def is_tuned(self):
+        '''check if string is tunned'''
+        if self._frequency == STRINGS[self._name]:
+            return True
+        else:
+            print('The right afination of string {} is {} Hz'.format(self._name, STRINGS[self._name]))
+            return False
