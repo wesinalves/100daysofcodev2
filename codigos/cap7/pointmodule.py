e6512b27f46ab4714fba95687a052b9446c6479d
WesinAlves <wesinec@gmail.com>
2020-05-18 22:18:17 -0300

    update class point

--- codigos/cap7/pointmodule.py
+++ codigos/cap7/pointmodule.py
@@ -1,23 +1,24 @@
-# cap 7 - Exemple 3: Point module
-# Definition and test function for class Point.
+'''
+How to program in Python - Chapter 7
+Definition and test function for class Point.
+'''
 
 class Point:
     """Class that represents a geometric point"""
 
-    def __init__( self, xValue = 0, yValue = 0 ):
+    def __init__(self, xValue=0, yValue=0):
         """Point constructor takes x and y coordinates"""
 
         self.x = xValue
         self.y = yValue
 
-    def __str__( self ):
+    def __str__(self):
         """String representation of a Point"""
+        return "({:d}, {:d} )".format(self.x, self.y)
 
-        return "({:d}, {:d} )".format( self.x, self.y )
-
-# main program
 def main():
-    point = Point( 72, 115 )
+    '''Main program'''
+    point = Point(72, 115)
 
     print("X coordinate is:", point.x)
     print("Y coordinate is:", point.y)
@@ -26,6 +27,7 @@ def main():
     point.y = 10
 
     print("The new location of point is:", point)
+
 
 if __name__ == "__main__":
     main()
