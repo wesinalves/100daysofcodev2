f2e055faafaa05980540006a4edfb717a662fec1
WesinAlves <wesinec@gmail.com>
2020-05-18 22:17:37 -0300

    update cylinder module

--- codigos/cap7/cylindermodule.py
+++ codigos/cap7/cylindermodule.py
@@ -1,36 +1,39 @@
-# cap 7 - Exemple 3: Cylinder module
-# Definition and test function for class Cylinder.
+'''
+How to program in Python - Chapter 7
+Definition and test function for class Cylinder.
+'''
 
 import math
 from pointmodule import Point
 from circlemodule import Circle
 
-class Cylinder( Circle ):
+
+class Cylinder(Circle):
     """Class that represents a cylinder"""
 
-    def __init__( self, x, y, radius, height ):
+    def __init__(self, x, y, radius, height):
         """Constructor for Cylinder takes x, y, height and radius"""
 
-        Circle.__init__( self, x, y, radius )
-        self.height = float( height )
+        Circle.__init__(self, x, y, radius)
+        self.height = float(height)
 
-    def area( self ):
+    def area(self):
         """Calculates (surface) area of a Cylinder"""
 
-        return 2 * Circle.area( self ) + 2 * math.pi * self.radius * self.height
+        return 2 * Circle.area(self) + 2 * math.pi * self.radius * self.height
 
-    def volume( self ):
-        return Circle.area( self ) * self.height
-    
-    def __str__( self ):
+    def volume(self):
+        """Calculates volume of a Cylinder"""
+        return Circle.area(self) * self.height
+
+    def __str__(self):
         """String representation of a Cylinder"""
+        return "{}; Height = {:f}".format(Circle.__str__(self), self.height)
 
-        return "{}; Height = {:f}".format( Circle.__str__( self ), self.height )
-
-# main program
 def main():
+    '''Main program'''
     # create object of class Cylinder
-    cylinder = Cylinder( 12, 23, 2.5, 5.7 )
+    cylinder = Cylinder(12, 23, 2.5, 5.7)
 
     # print Cylinder attributes
     print("X coordinate is:", cylinder.x)
@@ -46,12 +49,13 @@ def main():
     print("\nThe new points, radius and height of cylinder are:", cylinder)
     print("\nThe area of cylinder is: {:.2f}".format(cylinder.area()))
     print("\nThe volume of cylinder is: {:.2f}".format(cylinder.volume()))
-    
+
     # display the Cylinder as a Point
-    print("\ncylinder printed as a Point is:", Point.__str__( cylinder ))
-    
+    print("\ncylinder printed as a Point is:", Point.__str__(cylinder))
+
     # display the Cylinder as a Circle
-    print("\ncylinder printed as a Circle is:", Circle.__str__( cylinder ))
+    print("\ncylinder printed as a Circle is:", Circle.__str__(cylinder))
+
 
 if __name__ == "__main__":
     main()
