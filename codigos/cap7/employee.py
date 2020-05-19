be9cfcfe84cc81459c8af34b740c34962c20d1c6
WesinAlves <wesinec@gmail.com>
2020-05-18 22:04:13 -0300

    udpate class employee

--- codigos/cap7/employee.py
+++ codigos/cap7/employee.py
@@ -1,48 +1,54 @@
-# cap7 - Exemple 2:  The Employee class
-# Overriding base-class methods.
+'''
+How to program in Python - Chapter 7
+Overriding base-class methods.
+'''
 
 class Employee:
     """Class to represent an employee"""
 
-    def __init__( self, first, last ):
+    def __init__(self, first, last):
         """Employee constructor takes first and last name"""
-        self.firstName = first
-        self.lastName = last
-    
-    def __str__( self ):
+        self.first_name = first
+        self.last_name = last
+
+    def __str__(self):
         """String representation of an Employee"""
 
-        return ("{} {}".format(self.firstName, self.lastName ))
-    
+        return "{} {}".format(self.first_name, self.last_name)
 
-class HourlyWorker( Employee ):
+
+class HourlyWorker(Employee):
     """Class to represent an employee paid by hour"""
-    
-    def __init__( self, first, last, initHours, initWage ):
+
+    def __init__(self, first, last, init_hours, init_wage):
         """Constructor for HourlyWorker, takes first and last name,
         23 initial number of hours and initial wage"""
 
-        Employee.__init__( self, first, last )
-        self.hours = float( initHours )
-        self.wage = float( initWage )
+        Employee.__init__(self, first, last)
+        self.hours = float(init_hours)
+        self.wage = float(init_wage)
 
-    def getPay( self ):
+    def get_pay(self):
         """Calculates HourlyWorker's weekly pay"""
-        
+
         return self.hours * self.wage
-    
-    def __str__( self ):
+
+    def __str__(self):
         """String representation of HourlyWorker"""
 
-        print ("HourlyWorker.__str__ is executing")
+        print("HourlyWorker.__str__ is executing")
 
-        return "{} is an hourly worker with pay of ${:.2f}".format(Employee.__str__( self ), self.getPay())
-    
-# main program
-hourly = HourlyWorker( "Bob", "Smith", 40.0, 10.00 )
+        return "{} is an hourly worker with pay of ${:.2f}".format(Employee.__str__(self), \
+            self.get_pay())
 
-# invoke __str__ method several ways
-print("Calling __str__ several ways...")
-print(hourly)
-print(hourly.__str__())
-print(HourlyWorker.__str__(hourly))
+def main():
+    '''Main program'''
+    hourly = HourlyWorker("Bob", "Smith", 40.0, 10.00)
+
+    # invoke __str__ method several ways
+    print("Calling __str__ several ways...")
+    print(hourly)
+    print(hourly.__str__())
+    print(HourlyWorker.__str__(hourly))
+
+main()
