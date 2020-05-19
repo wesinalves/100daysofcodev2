5e79cba3439a7a8c0debeb41b58bc14272833735
WesinAlves <wesinec@gmail.com>
2020-05-18 22:17:24 -0300

    circle module

--- codigos/cap7/circlemodule.py
+++ codigos/cap7/circlemodule.py
@@ -1,30 +1,32 @@
-# cap 7 - Exemple 3: Circle module
-# Definition and test function for class Circle.
+'''
+How to program in Python - Chapter 7
+Definition and test function for class Circle.
+'''
+
 import math
 from pointmodule import Point
 
-class Circle( Point ):
+
+class Circle(Point):
     """Class that represents a circle"""
 
-    def __init__( self, x = 0, y = 0, radiusValue = 0.0 ):
+    def __init__(self, x=0, y=0, radius_value=0.0):
         """Circle constructor takes center point and radius"""
 
-        Point.__init__( self, x, y ) # call base-class constructor
-        self.radius = float( radiusValue )
+        Point.__init__(self, x, y)  # call base-class constructor
+        self.radius = float(radius_value)
 
-    
-    def area( self ):
+    def area(self):
+        """Compute area of circle"""
         return math.pi * self.radius ** 2
-    
-    def __str__( self ):
+
+    def __str__(self):
         """String representation of a Circle"""
-
-        return "Center = {} Radius = {:.2f}".format( Point.__str__( self ), self.radius )
-
-# main program
+        return "Center = {} Radius = {:.2f}".format(Point.__str__(self), self.radius)
 
 def main():
-    circle = Circle( 37, 43, 2.5 )
+    '''Main program'''
+    circle = Circle(37, 43, 2.5)
 
     # print circle attributes
     print("X coordinate is:", circle.x)
@@ -38,7 +40,8 @@ def main():
 
     print("\nThe new location and radius of circle are:", circle)
     print("The area of circle is: {:.2f}".format(circle.area()))
-    print("\ncircle printed as a Point is:", Point.__str__( circle ))
+    print("\ncircle printed as a Point is:", Point.__str__(circle))
+
 
 if __name__ == "__main__":
     main()
