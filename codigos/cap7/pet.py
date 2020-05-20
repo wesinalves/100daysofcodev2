61ba4ddba4248d5524997b0be504746024a70c60
WesinAlves <wesinec@gmail.com>
2020-05-19 21:27:30 -0300

    add class pet

---
+++ codigos/cap7/pet.py
@@ -0,0 +1,87 @@
+'''
+How to program in Python - Chapter II
+Creating a class hierarchy with an abstract base class.
+'''
+
+import math
+
+class Pet:
+    '''Abstract base class for pet'''
+
+    def __init__(self, name, age):
+        '''Construtor for class pet'''
+        if self.__class__ == Pet:
+            raise NotImplementedError('Cannot create object of class Employee')
+        self.name = name
+        self.age = age
+    
+    def __str__(self):
+        '''String representation of Pet'''
+        return '{:s} {:d}'.format(self.name, self.age)
+    
+    def food(self):
+        '''Abstract method to comput how much food it requires'''
+        raise NotImplementedError('Cannot call abstract method')
+    
+    def _human_age(self, value):
+        '''Utility method to compute human age'''
+        return math.log(value) * 16 + 31
+ 
+class Dog(Pet):
+    '''Abstract class for Dog'''
+
+    def __init__(self, name, age, weight):
+        '''Constructor for class Dog'''
+        if self.__class__ == Dog:
+            raise NotImplementedError('Cannot create object of class Employee')
+        Pet.__init__(self, name, age)
+        self.weight = weight
+        self.human_age = self._human_age(int(age))
+    
+    def food(self):
+        '''Compute how much food it requires'''
+        required = 0
+
+        if 0 < self.weight < 3:
+            required = self.weight * 7 / 100
+        elif 3 < self.weight < 5:
+            required = self.weight * 6 / 100
+        elif 5 < self.weight < 10:
+            required = self.weight * 5 / 100
+        elif 10 < self.weight < 22:
+            required = self.weight * 4 / 100
+        else:
+            required = self.weight * 3.5 / 100
+        
+        return '{} Kg'.format(required)
+    
+    def __str__(self):
+        '''String representation of Dog'''
+        return '{:17s} {:s}'.format('Dog', Pet.__str__(self))
+    
+class Beagle(Dog):
+    '''Concrete class,  inherits from Pet'''
+
+    def __init__(self, *args, **kwargs):
+        '''Beagle constructor, takes a list and a dict as argument'''
+        Dog.__init__(self, kwargs['name'], kwargs['age'], kwargs['weight'])
+        self.height = kwargs['height']
+        self.width = kwargs['width']
+        self.colors = args
+
+    def __str__(self):
+        '''String representation of Beagle'''
+        return '{}: {:10s}'.format(Dog.__str__(self), 'Beagle')
+    
+    def _human_age(self, value):
+        '''Overide method for class beagle'''
+        return int(math.log(value) * 12 + 31)
+
+def main():
+    '''Main program'''
+    dog_dict = {'name':'snoop', 'age':3, 'weight':13, 'height':15, 'width':30}
+    colors = ['black', 'white', 'brown']
+    dog = Beagle(colors, **dog_dict)
+    print(dog, dog._human_age(dog.age), dog.food())
+
+main()
