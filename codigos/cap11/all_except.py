e50f05a184edf09d7d904a5fb4659793f740d0d7
Wesin Ribeiro Alves <wesinec@gmail.com>
2023-03-03 10:46:08 -0300

    refactoring all exception example

--- codigos/cap11/all_except.py
+++ codigos/cap11/all_except.py
@@ -1,31 +1,41 @@
+'''
+How to program in Python - Chapter 11
+Catch all exception example.
+'''
+
 import random
 
-for i in range(10):
-    # Loop 10 times
-    print('####### Beginning of loop iteration', i)
-    try:
-        r = random.randint(1, 5)
-        # r is pseudorandomly 1, 2, 3, or 4
-        if r == 1:
-            print(int('um')) # Try to convert a non-integer
-        elif r == 2:
-            [][2] = 5
-        # Try to assign to a nonexistent index of the empty list
-        elif r == 3:
-            print({}[1]) # Try to use a nonexistent key to get an item from a dictionary
-        elif r == 4:
-            print(3/0) # Try to divide by zero
-        else:
-            print(x)
-    except ValueError:
-        print('Cannot convert integer')
-    except IndexError:
-        print('List index is out of range')
-    except ZeroDivisionError:
-        print('Division by zero not allowed')
-    except NameError:
-        print('Variable must be declared')
-    except:
-         # Catch absolutely any other type of exception
-        print('This program has encountered a problem')
+def main():
+    '''Catch all exceptions.'''
+    for i in range(10):
+        # Loop 10 times
+        print('####### Beginning of loop iteration', i)
+        try:
+            r = random.randint(1, 5)
+            # r is pseudorandomly 1, 2, 3, or 4
+            if r == 1:
+                print(int('um')) # Try to convert a non-integer
+            elif r == 2:
+                [][2] = 5
+            # Try to assign to a nonexistent index of the empty list
+            elif r == 3:
+                print({}[1]) # Try to use a nonexistent key to get an item from a dictionary
+            elif r == 4:
+                print(3/0) # Try to divide by zero
+            else:
+                print(x)
+        except ValueError:
+            print('Cannot convert integer')
+        except IndexError:
+            print('List index is out of range')
+        except ZeroDivisionError:
+            print('Division by zero not allowed')
+        except NameError:
+            print('Variable must be declared')
+        except:
+            # Catch absolutely any other type of exception
+            print('This program has encountered a problem')
+
+if __name__ == '__main__':
+    main()
     
