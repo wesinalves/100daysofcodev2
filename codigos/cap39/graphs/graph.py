4d1cdea653832842bd9491ae190efdbff8068954
Wesin Alves <wesinec@gmail.com>
2024-09-11 23:40:25 -0300

    fix dfs_visit procedure to take account vertex reference

--- codigos/cap39/graphs/graph.py
+++ codigos/cap39/graphs/graph.py
@@ -18,23 +18,24 @@ class Graph:
         self.vertex_list = [[] for _ in range(num_vertex)] # adjacent list
         self.time = 0
     
-    def add_edges(self, u: int, v: int):
-        vertex_u = Vertex(u, 'white', None)
-        vertex_v = Vertex(v, "white", None)
+    def add_edges(self, u: Vertex, v: Vertex, non_direct = True):
+        #vertex_u = Vertex(u, 'white', None)
+        #vertex_v = Vertex(v, 'white', None)
         # insert u if not in list
-        uids = [x.id for x in self.vertex_list[u]]
-        if u not in uids:
-            self.vertex_list[u].append(vertex_u)
-        if v not in uids:
-            self.vertex_list[u].append(vertex_v)
+        uids = [x.id for x in self.vertex_list[u.id]]
+        if u.id not in uids:
+            self.vertex_list[u.id].append(u)
+        if v.id not in uids:
+            self.vertex_list[u.id].append(v)
 
-        # works for non direct graph only
-        vids = [y.id for y in self.vertex_list[v]]
-        if v not in vids:
-            self.vertex_list[v].append(vertex_v)
-        if u not in vids:
-            self.vertex_list[v].append(vertex_u)
-    
+        if non_direct:
+            # works for non direct graph only
+            vids = [y.id for y in self.vertex_list[v.id]]
+            if v.id not in vids:
+                self.vertex_list[v.id].append(v)
+            if u.id not in vids:
+                self.vertex_list[v.id].append(u)
+        
     def get_neighbor(self, u: Vertex):
         # the first vertex is out
         return self.vertex_list[u.id][1:]
@@ -43,44 +44,76 @@ class Graph:
         for adj in self.vertex_list:
             u = adj[0]
             if u.color == 'white':
-                self.dfs_visit(self, u)
+                self.dfs_visit(u)
+    
+    def dfs_from_source(self, s: int):
+        self.time = 0
+        u = self.vertex_list[s][0]
+        if u.color == 'white':
+            self.dfs_visit(u)
 
     def dfs_visit(self, u: Vertex):
         self.time += 1
-        u.d = self.time
-        u.color = 'gray'
-        for v in self.get_neighbor(u):
+        #u.d = self.time
+        self.vertex_list[u.id][0].d = self.time
+        self.vertex_list[u.id][0].color = 'gray'
+        adjs = self.get_neighbor(u)
+        for v in adjs:
             if v.color == 'white':
-                v.parent = u
-                self.dfs_visit(self, v)
-        u.color = 'black'
+                self.vertex_list[v.id][0].parent = u
+                self.dfs_visit(v)
+        self.vertex_list[u.id][0].color = 'black'
         self.time += 1
-        u.f = self.time
+        self.vertex_list[u.id][0].f = self.time
 
 if __name__ == "__main__":
-    G = Graph(16, 15)
-    G.add_edges(0, 4)
-    G.add_edges(2, 3)
-    G.add_edges(6, 2)
-    G.add_edges(8, 9)
-    G.add_edges(8, 12)
-    G.add_edges(10, 9)
-    G.add_edges(14, 15) 
-    G.add_edges(14, 10)
-    G.add_edges(6, 5)
-    G.add_edges(10, 11)
-    G.add_edges(11, 7)
-    G.add_edges(4, 8)
-    G.add_edges(0, 1)
-    G.add_edges(1, 2)
-    G.add_edges(12, 13)
+    # G = Graph(16, 15)
+    # G.add_edges(0, 4)
+    # G.add_edges(2, 3)
+    # G.add_edges(6, 2)
+    # G.add_edges(8, 9)
+    # G.add_edges(8, 12)
+    # G.add_edges(10, 9)
+    # G.add_edges(14, 15) 
+    # G.add_edges(14, 10)
+    # G.add_edges(6, 5)
+    # G.add_edges(10, 11)
+    # G.add_edges(11, 7)
+    # G.add_edges(4, 8)
+    # G.add_edges(0, 1)
+    # G.add_edges(1, 2)
+    # G.add_edges(12, 13)
 
-    print(G.vertex_list[0])
+    G = Graph(6, 8)
+    vertex0 = Vertex(0, 'white', None)
+    vertex1 = Vertex(1, 'white', None)
+    vertex2 = Vertex(2, 'white', None)
+    vertex3 = Vertex(3, 'white', None)
+    vertex4 = Vertex(4, 'white', None)
+    vertex5 = Vertex(5, 'white', None)
 
-    for v in range(G.num_vertex):
-        for u in G.vertex_list[v]:
-            print(f'{u.id} -> ', end='\t')
-        print()
+    G.add_edges(vertex0, vertex1, False)
+    G.add_edges(vertex0, vertex3, False)
+    G.add_edges(vertex1, vertex4, False)
+    G.add_edges(vertex2, vertex4, False)
+    G.add_edges(vertex2, vertex5, False)
+    G.add_edges(vertex3, vertex1, False)
+    G.add_edges(vertex4, vertex3, False)
+    G.add_edges(vertex5, vertex5, False)
+
+
+    G.dfs_from_source(0)
+    #G.dfs()
+
+    print(G.time)
+    print(G.vertex_list[0][0].d)
+    print(G.vertex_list[0][0].f)
+    print(G.vertex_list[0][0].color)
+
+    # for v in range(G.num_vertex):
+    #     for u in G.vertex_list[v]:
+    #         print(f'{u.id} -> ', end='\t')
+    #     print()
 
 
 
